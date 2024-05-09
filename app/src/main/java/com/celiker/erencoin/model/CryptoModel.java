44fe68a5922df9b69bf6d1c5354db3cb6eff10c0
Eren Yurtaslan <eren51abdulkadir@gmail.com>
2024-05-09 23:52:11 +0300

    app files are added.

---
+++ app/src/main/java/com/celiker/erencoin/model/CryptoModel.java
@@ -0,0 +1,16 @@
+package com.celiker.erencoin.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class CryptoModel {
+
+    @SerializedName("currency")
+   public  String currency;
+
+    @SerializedName("price")
+    public String price;
+
+
+
+
+}
---
+++ app/src/main/java/com/celiker/erencoin/service/CryptoAPI.java
@@ -0,0 +1,19 @@
+package com.celiker.erencoin.service;
+
+import com.celiker.erencoin.model.CryptoModel;
+
+import java.util.List;
+
+import io.reactivex.Observable;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface CryptoAPI {
+
+    @GET("atilsamancioglu/K21-JSONDataSet/master/crypto.json")
+    Observable<List<CryptoModel>> getData();
+    //Call<List<CryptoModel>> getData();
+
+
+
+}
